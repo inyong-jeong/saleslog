9c4083b7bd026f35ec8f510ebbb4e246f4f864b5
inyong_jeong <jeong7240@theklab.co>
2021-08-19 17:51:51 +0900

    feat: add handleonSubmit in SignUp page

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,183 +1,288 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 import { connect } from 'react-redux';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import PolicyLayout from 'components/auth/PolicyLayout';
-import UserTypeLayout from 'components/auth/UserTypeLayout';
-import UserFormLayout from 'components/auth/UserFormLayout';
-import SignUpCompleteLayout from 'components/auth/SignUpCompleteLayout';
-import TableIndicator from 'components/auth/TableIndicator';
-import { getCi } from 'helpers/domainUtils';
-import { Link } from 'react-router-dom';
-import { setPolicyCheck, setUserType, setUserForm, signUpUser } from 'redux/actions';
+import { Spinner } from "reactstrap";
+import { Helmet } from "react-helmet";
+import { getCi } from "helpers/domainUtils";
+import { regex } from 'constants/regex';
+import useInput from 'hooks/useInput';
+import { postRegisteration, postAuthNumber } from 'redux/actions';
 
+import { proxyPath } from '@theklab/saleslog/src/proxy';
 
-const SignUpRoute = ({ component: Component, handler: handlerProps, comepletedState: comepletedState, index: index, ...rest }) => {
-  // access to prior
-  if (index === 0 && comepletedState[0]) {
-    return <Redirect to="/signup/usertype" />
-  } else if (index === 1 && comepletedState[1]) {
-    return <Redirect to="/signup/form" />
-  } else if (index === 2 && comepletedState[2]) {
-    return <Redirect to="/signup/complete" />
+import StyledCheckbox from 'components/styledcomponent/Checkbox'
+import StyledInput from 'components/styledcomponent/Input';
+import StyledButton from 'components/styledcomponent/Button';
+import RoundInputField from "components/RoundInputField";
+import RoundHalfInputField from "components/RoundHalfInputField";
+
+function SignIn(props) {
+  const [alaramText, setAlarmText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [viewHeight, setViewHeight] = useState(window.innerHeight);
+
+  // 회원가입 데이터
+  const [useremail, onChangeId] = useInput('');
+  const [password, onChangePassword] = useInput('')
+  const [passwordcheck, onChangePasswordCheck] = useInput('')
+  const [firstname, onChangeFirstName] = useInput('');
+  const [lastname, onChangeLastName] = useInput('')
+  const [authnumber, onChangeAuthNumber] = useInput('')
+  const [comp_name, onChangeCompName] = useInput('')
+  const [comp_domain, onChangeCompDomain] = useInput('')
+
+  //조건 오류
+  const [term, setTerm] = useState(false)
+  const [termError, setTermError] = useState()
+  const [emailerror, setEmailError] = useState();
+  const [authnumbererror, setAuthNumberError] = useState();
+  const [passworderror, setPassWordError] = useState();
+  const [compnameerror, setCompNameError] = useState();
+  const [compdomainerror, setCompDomainError] = useState();
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowDimensions);
+
+    return () => {
+      window.removeEventListener("resize", updateWindowDimensions);
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   if (props.error) {
+  //     setLoading(false);
+  //   }
+  // }, [props.error]);
+
+  const onChangeTerm = useCallback((e) => {
+    console.log('checked : ', e.target.checked);
+    setTerm(e.target.checked);
+  }, []);
+
+  const updateWindowDimensions = () => {
+    setViewHeight(window.innerHeight);
+  };
+
+  // const handleOnClick = () => {
+  //   props.history.push('/signup')
+  // }
+
+  const handleLandingPage = () => {
+    props.history.push('/');
   }
 
-  // access to forward
-  if (comepletedState[0] === false && index > 0) {
-    return <Redirect to="/signup/policy" />
-  } else if (comepletedState[1] === false && index > 1) {
-    return <Redirect to="/signup/usertype" />
-  } else if (comepletedState[2] === false && index > 2) {
-    return <Redirect to="/signup/form" />
-  }
-  return (
-    <Route
-      {...rest}
-      render={routeProps => (
-        <Component {...routeProps} {...handlerProps} />
-      )}
-    />
-  );
-}
-
-function SignUp(props) {
-  const handlePolicyCheckSubmit = () => {
-    props.setPolicyCheck(true);
-  }
-
-  const handleUserTypeSubmit = (userType) => {
-    props.setUserType(userType);
-  }
-
-  const handleUserFormSubmit = (userForm) => {
-    if (props.userType === "manager") {
-      props.signUpUser("manager", userForm);
-    } else if (props.userType === "member") {
-      props.signUpUser("member", userForm);
+  const handleOnPostAuthNumber = () => {
+    if (new RegExp(regex.email).exec(useremail)) {
+      props.postAuthNumber(useremail)
+    } else {
+      setEmailError("이메일 형식이 잘못되었습니다");
     }
   }
 
-  const handleSignUpComplete = () => {
-    props.setPolicyCheck(false);
-    props.setUserType(null);
-    props.setUserForm({});
-    setSignUp([...INIT_STATE]);
-    props.history.replace('/signin');
-  }
-
-  const INIT_STATE = [
-    {
-      to: '/signup/policy',
-      title: '1. 이용약관 동의',
-      complete: false,
-      component: PolicyLayout,
-      onClick: null,
-      handler: {
-        handleSubmit: handlePolicyCheckSubmit
-      }
-    },
-    {
-      to: '/signup/usertype',
-      title: '2. 사용자 구분',
-      complete: false,
-      component: UserTypeLayout,
-      onClick: null,
-      handler: {
-        handleSubmit: handleUserTypeSubmit
-      }
-    },
-    {
-      to: '/signup/form',
-      title: '3. 회원정보',
-      complete: false,
-      onClick: null,
-      component: UserFormLayout,
-      handler: {
-        handleSubmit: handleUserFormSubmit
-      }
-    },
-    {
-      to: '/signup/complete',
-      title: '4. 가입완료',
-      complete: false,
-      component: SignUpCompleteLayout,
-      handler: {
-        handleSubmit: handleSignUpComplete
-      }
-    }
-  ];
-
-  const [signup, setSignUp] = useState(INIT_STATE);
-
-  const setComplete = (index) => {
-    let temp = [...signup];
-    temp[index].complete = true;
-    setSignUp(temp);
-  }
-
-  const componentDidMount = () => {
-    signup[0].complete = props.policyCheck;
-    signup[1].complete = props.userType !== null ? true : false;
-    setSignUp([...signup]);
-  }
-
-  const onPolicyCheck = () => {
-    if (props.policyCheck === true) {
-      setComplete(0);
-      props.history.replace('/signup/usertype');
+  const handleOnCheckNumber = () => {
+    if (Number(authnumber) === props.authNumberResponse) {
+      setAuthNumberError('인증이 완료되었습니다.')
+    } else {
+      setAuthNumberError('인증번호가 틀렸습니다.')
     }
   }
 
-  const onUserTypeCheck = () => {
-    if (props.userType) {
-      setComplete(1);
-      props.history.replace('/signup/form');
+  const handleOnSubmit = () => {
+    if (password !== passwordcheck) {
+      setPassWordError('비밀번호가 일치하지 않습니다.')
+    } else if (!(new RegExp(regex.password).exec(password))) {
+      setPassWordError('비밀번호 형식이 잘못되었습니다.')
+    } else if (term === false) {
+      setTermError('약관에 동의하여 주세요.')
+    } else if (Number(authnumber) !== props.authNumberResponse) {
+      setAuthNumberError('인증번호가 틀렸습니다.')
+    } else if (!(new RegExp(regex.email).exec(useremail))) {
+      setEmailError("이메일 형식이 잘못되었습니다");
+    } else if (comp_name.length >= 20) {
+      setCompNameError('이름은 최대 20자 이내여야 합니다.')
+    } else if (comp_domain.lengh >= 20) {
+      setCompDomainError('영문자, 숫자, 대시(-)를 포함해 4~20자 이내여야 합니다.')
+    }
+    else {
+      props.postRegisteration(useremail, password, firstname, lastname, comp_name, comp_domain)
     }
   }
 
-  const onSignUpSuccess = () => {
-    if (props.signUpResponse) {
-      setComplete(2);
-      props.history.replace('/signup/complete');
-    }
+  // const handleOnSubmit = () => {
+  //   props.postRegisteration('jeong@theklab.co', '1111', '인용', '정', 'comp_name', 'comp-domain')
+  // }
+
+  // 컴포넌트 스타일링
+
+  const ViewStyle = {
+    height: viewHeight,
+    flexDirection: 'row',
+    display: 'flex',
+    justifyContent: 'center',
+  };
+
+  const CardStyle = {
+    flexDirection: 'column',
+    alignItems: 'center',
+    display: 'flex'
   }
 
-  useEffect(componentDidMount, []);
-  useEffect(onPolicyCheck, [props.policyCheck]);
-  useEffect(onUserTypeCheck, [props.userType]);
-  useEffect(onSignUpSuccess, [props.signUpResponse]);
+  const ImgStyle = {
+    textAlign: 'center',
+    cursor: 'pointer'
+  }
+
+  const CheckBoxStyle = {
+    fontSize: '16px',
+    color: 'black'
+  }
+
+  const ButtonStyle = {
+    width: '104px',
+    marginLeft: '7px'
+  }
+
   return (
     <React.Fragment>
-      <div className="container">
-        <div className="row">
-          <div className="col-3 table-indicator justify-content-center">
-            <TableIndicator
-              className="position-fixed"
-              table={signup}
-              current={parseInt(signup.map((v, i) => v.to === props.location.pathname ? i.toString() : false).filter((v) => v)[0])}
-            />
-          </div>
-          <div className="col-md-9 col-lg-6 signup-header">
-            <div className="text-center mb-4">
-              <img src={getCi()} className="auth-logo" alt="logo" height="72" />
+      <Helmet>
+        <title>로그인</title>
+      </Helmet>
+      <div className="container-md">
+        <div className="row" style={ViewStyle}>
+          <div className="col-xxl-4 col-xl-4 col-lg-2 col-md-2 col-sm-2"></div>
+          <div className="col-xxl-4 col-xl-4 col-lg-8 col-md-8 col-sm-8 align-self-center ">
+            <div className="card">
+              <div className="card-body" style={CardStyle}>
+                <div style={ImgStyle}>
+                  <img src={getCi()} className="auth-logo mb-3" alt="logo" onClick={handleLandingPage} />
+                </div>
+                <form>
+                  <div className='mb-3'>
+                    <h2><strong>회원가입</strong></h2>
+                  </div>
+                  <div className="form-group">
+                    <RoundHalfInputField
+                      id="user_name"
+                      title="이름"
+                      placeholder="이름"
+                      value={firstname}
+                      onChange={onChangeFirstName}
+                    />
+                    <span> </span>
+                    <RoundHalfInputField
+                      id="user_last_name"
+                      title="성"
+                      placeholder="성"
+                      value={lastname}
+                      onChange={onChangeLastName}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <StyledInput
+                      style={{ width: '232px', fontSize: '16px' }}
+                      id="user_email"
+                      title="이메일"
+                      placeholder="customer@example"
+                      value={useremail}
+                      onChange={onChangeId}
+                    />
+                    <StyledButton style={ButtonStyle} onClick={handleOnPostAuthNumber}>인증번호 전송</StyledButton>
+                    {emailerror && <p className="text-danger mt-2">{emailerror}</p>}
+
+                  </div>
+                  <div className="form-group">
+                    <StyledInput
+                      style={{ width: '232px', fontSize: '16px' }}
+                      id="authnumber"
+                      title="인증번호"
+                      placeholder="인증번호 입력"
+                      value={authnumber}
+                      onChange={onChangeAuthNumber}
+                    />
+                    <StyledButton style={ButtonStyle} onClick={handleOnCheckNumber}>확인</StyledButton>
+                    {authnumbererror && <p className="text-danger mt-2">{authnumbererror}</p>}
+                  </div>
+                  <div className="form-group">
+                    <RoundInputField
+                      id="password"
+                      title="비밀번호"
+                      placeholder="비밀번호 입력(특수문자 포함 8자리 이상)"
+                      value={password}
+                      type="password"
+                      onChange={onChangePassword}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <RoundInputField
+                      id="password"
+                      title="비밀번호"
+                      placeholder="비밀번호 확인"
+                      type='password'
+                      value={passwordcheck}
+                      onChange={onChangePasswordCheck}
+                    />
+                    {passworderror && <p className="text-danger mt-2">{passworderror}</p>}
+                  </div>
+                  <h4 style={{ marginBottom: '4px' }}> <strong>동료들과 함께 일하고 소통할 워크그룹을 <br /> 만들어보세요</strong></h4>
+                  <div className="form-group">
+                    <RoundInputField
+                      id="workgroup"
+                      title="워크그룹"
+                      placeholder="워크그룹 이름"
+                      value={comp_name}
+                      onChange={onChangeCompName}
+                    />
+                    {compnameerror && <p className="text-danger mt-2">{compnameerror}</p>}
+                  </div>
+                  <div className="form-group">
+                    <StyledInput
+                      style={{ width: '230px', fontSize: '16px' }}
+                      id="domain"
+                      title="도메인"
+                      placeholder="your-workspace-url"
+                      value={comp_domain}
+                      onChange={onChangeCompDomain}
+                    />
+                    <span style={{ fontSize: '16px', color: 'black' }}>.saleslog.co</span>
+                    {compdomainerror && <p className="text-danger mt-2">{compdomainerror}</p>}
+                  </div>
+                  <div className="form-group mt-3">
+                    <StyledCheckbox
+                      onChange={onChangeTerm}
+                      style={CheckBoxStyle}>
+                      동의 버튼을 선택하면 세일즈로그 서비스이용 약관,< br /> 위치정보서비스 약관, 개인정보처리 방침에 동의하게< br /> 됩니다.
+                    </StyledCheckbox>
+                    {(!term) && <h5 className='text text-danger'> 약관에 동의하여 주세요.</h5>}
+                  </div>
+                  <div className="form-group">
+                    <button
+                      className="btn btn-outline-primary"
+                      style={{ width: '343px', height: '48px' }}
+                      disabled={loading}
+                      onClick={handleOnSubmit}
+                    >
+                      다음
+                    </button>
+                  </div>
+                </form>
+                <div style={{ textAlign: 'center', fontColor: 'black' }}>
+                </div>
+              </div>
             </div>
-            <Switch>
-              {signup.map((v, i) =>
-                <SignUpRoute key={v.to} path={v.to} component={v.component} handler={v.handler} index={i} comepletedState={signup.map((v) => v.complete)} />
-              )}
-            </Switch>
           </div>
+          <div className="col-xxl-4 col-xl-4 col-lg-2 col-md-2 col-sm-2"></div>
         </div>
-        <footer style={{ height: '128px' }}>
-        </footer>
-        <Link to='/signin'>돌아가기</Link>
       </div>
-    </React.Fragment>
+    </React.Fragment >
   );
 }
 
 const mapStateToProps = (state) => {
-  const { policyCheck, userType, userForm, signUpResponse } = state.Auth;
-  return { policyCheck, userType, userForm, signUpResponse };
-}
+  const { error, authNumberResponse, authNumberError } = state.Auth;
+  return { error, authNumberResponse, authNumberError };
+};
 
-export default connect(mapStateToProps, { setPolicyCheck, setUserType, setUserForm, signUpUser: signUpUser.call })(SignUp);
+const mapStateToDispatch = {
+  postRegisteration: postRegisteration.call,
+  postAuthNumber: postAuthNumber.call
+}
+export default connect(mapStateToProps, mapStateToDispatch)(SignIn);
