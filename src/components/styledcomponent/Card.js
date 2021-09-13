6c25c8859df562686599a403b0c35074518d4699
inyong_jeong <jeong7240@theklab.co>
2021-09-13 15:37:16 +0900

    style: add change styles

--- src/components/styledcomponent/Card.js
+++ src/components/styledcomponent/Card.js
@@ -3,8 +3,8 @@ import { Card } from 'antd';
 import Standard from 'components/standard';
 
 const StyledCard = styled(Card)`
-border: 1px solid black;
-width: ${Standard.size.width};
+border: 1px solid #DDDDDD;
+// width: ${Standard.size.width};
 `;
 
 export default StyledCard;
