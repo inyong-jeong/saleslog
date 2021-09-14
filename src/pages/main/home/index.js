68ab3365a366336de48c1564e9511bcd2608bb35
inyong_jeong <jeong7240@theklab.co>
2021-09-14 17:56:18 +0900

    chore: delete useless lines

--- src/pages/main/home/index.js
+++ src/pages/main/home/index.js
@@ -1,32 +1,16 @@
 import React from "react";
-import { connect } from "react-redux";
 import { Helmet } from "react-helmet";
 import MobileMenu from 'components/MobileMenu';
 
+
 function DashBoard(props) {
-
-  let k = startCountDown(3600)
-
-  function startCountDown(duration) {
-
-    let secondsRemaining = duration;
-
-    setInterval(function () {
-      secondsRemaining = secondsRemaining - 1;
-    }, 1000);
-    return secondsRemaining
-
-  }
-
-  // startCountDown(3600);
-
 
   return (
     <React.Fragment>
       <Helmet>
         세일즈로그 - 메인페이지
       </Helmet>
-      <div>{k}</div>
+      <div>홈 화면</div>
       <div className="container-fluid">
         <div className="row">
           <div className="col-12">
@@ -36,6 +20,7 @@ function DashBoard(props) {
       <div className="row">
         <MobileMenu />
       </div>
+
     </React.Fragment>
   );
 }
@@ -43,3 +28,8 @@ function DashBoard(props) {
 
 
 export default (DashBoard);
+
+
+
+
+
