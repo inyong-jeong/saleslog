f50fa0c0d18633e5376ab78b89662f5569619f3d
inyong_jeong <jeong7240@theklab.co>
2021-09-29 20:57:07 +0900

    feat: add needs cards

---
+++ src/components/NeedsCard.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Collapse, Button, CardBody, Card, CardTitle } from 'reactstrap';
+
+export default function NeedsCard(props) {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [sentence, setSentence] = useState(null);
+  const toggle = () => setIsOpen(!isOpen);
+  useEffect(() => {
+    setSentence(props.sentences.split('\n'))
+  }, [])
+  return (
+    <React.Fragment>
+      <div>
+        <Button color="secondary" onClick={toggle} style={{ marginBottom: '1rem' }}>{`${props.needs}니즈`}</Button>
+        <Collapse isOpen={isOpen}>
+          <Card>
+            <CardBody>
+              <CardTitle tag="h4">{`${props.needs}니즈 코칭이 필요합니다.`}</CardTitle>
+              {sentence &&
+                sentence.map((v) => {
+                  return (
+                    <div className="row" id="app">
+                      <div className="col-2">
+                        {/* <div className={`avatar-md bg-${props.needs} border-${props.needs}`}>
+                          <i className={props.icon + " font-22 avatar-title"}> </i>
+                        </div> */}
+                      </div>
+                      <div className="col-8">
+                        <a href="javascropt:void(0)" className="text-reset mb-2 d-block">
+                          <i className={`mdi mdi-checkbox-blank-circle-outline mr-1 text-${props.needs}`}></i>
+                          <span className="mb-0 mt-1 text-dark text-bold font-14">{v}</span>
+                        </a>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </CardBody>
+          </Card>
+        </Collapse>
+      </div>
+    </React.Fragment>
+  );
+}
