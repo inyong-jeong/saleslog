ff6d1572db295ea4b17a2c3f44b5a8cf854d0276
yechubot <yechukim217@gmail.com>
2021-10-13 15:28:12 +0900

    fix: add correct key for notice list

--- src/components/etc/notice/NoticeItems.js
+++ src/components/etc/notice/NoticeItems.js
@@ -1,33 +1,19 @@
-import React, { useEffect, useState } from 'react';
-
+import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import { Divider } from 'antd';
-import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import { useDispatch, useSelector } from "react-redux";
-//import { getAllCustomer } from 'redux/customer/actions';
 import { base64Enc } from 'constants/commonFunc';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { useHistory, useLocation } from 'react-router';
+import { useHistory } from 'react-router';
 
 const NoticeItems = ({ page, setPage, data, loading, noticeType }) => {
 
-  const dispatch = useDispatch()
   const history = useHistory()
-  let restCount
-
-  useEffect(() => {
-    console.log('data::::::::::::::::::::::', data)
-    //dispatch(getAllCustomer.call(inputs, 1))
-  }, [])
 
   const handleNextPage = () => {
-    //if (listCounts >= cusotomerList.length) {
-    // console.log('page::::::::::::', page)
     if (loading == true) return
     setPage(page + 1)
-    //}
   }
 
   return (
@@ -35,16 +21,18 @@ const NoticeItems = ({ page, setPage, data, loading, noticeType }) => {
       hasMore={true}
       dataLength={20}
       next={handleNextPage}>
-      {/* {loading && <CircularProgress />} */}
+
       <List>
         {data ?
-          data.map((singleList, index) => {
+          data.map(singleList => {
             return (
               <div onClick={() => {
                 (noticeType === 'group') ?
                   history.push({ pathname: `/main/etc/notice/group/detail/${base64Enc(singleList.b_idx)}/` }) :
                   history.push({ pathname: `/main/etc/notice/system/detail/${base64Enc(singleList.b_idx)}/` })
-              }} key={singleList.num}>
+              }}
+                key={singleList.b_idx}
+              >
                 <ListItem style={{ height: 50, cursor: 'pointer', color: '#333' }}>
                   <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'stretch' }}>
                     <p style={{ width: '80%', fontSize: 14, fontWeight: 500, marginBottom: 0 }}>{singleList.title}</p>
