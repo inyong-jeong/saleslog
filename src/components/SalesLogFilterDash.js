6f71f12536e186a565eae5ceca11b699b43ff6da
yechubot <yechukim217@gmail.com>
2021-10-13 18:38:39 +0900

    fix: change max tag number on filterdash

--- src/components/SalesLogFilterDash.js
+++ src/components/SalesLogFilterDash.js
@@ -1,66 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { TreeSelect, Row, Col, Select } from 'antd';
-// import Select from 'react-select';
-
 import { connect } from 'react-redux';
 import {
   getorganizationDash, getorganizationusersDash
 } from 'redux/actions';
-import cmm from 'constants/common';
-
-
+import { useMediaQuery } from "react-responsive";
 
 const SalesLogFilterDash = (props) => {
+
+  const isMobile = useMediaQuery({
+    query: "(max-width:1190px)"
+  });
+
   const [selectedOrganization, setSelectedOrganization] = useState(undefined);
-  const [selectedOrganization1, setSelectedOrganization1] = useState(undefined);
-  const [selectedOrganization2, setSelectedOrganization2] = useState(undefined);
-  const [selectedOrganization3, setSelectedOrganization3] = useState(undefined);
   const [selId, setSelId] = useState(props.id);
   const [selIdUser, setSelIdUser] = useState(props.id);
-
   const [treedata, setTreedata] = useState([])
-
-  const [chgCombo, setChgCombo] = useState(0);
   const [selectedOrganizationuser, setSelectedOrganizationUser] = useState(undefined);
-
   const [selectedItems, setSelectedItems] = useState([])
   const [filteredlist, setFilteredlist] = useState([])
   const [filteredOptions, setFilteredOptions] = useState([])
-  //const filteredlist = props.organizationuserDashRes && props.organizationuserDashRes.map(v => v.user_name);
-
-
-
-  const salesActivityOption =
-    [{ label: '선택없음', value: '' },
-    { label: '니즈조사', value: '0030001' },
-    { label: '동향/정보수집', value: '0030002' },
-    { label: '제안', value: '0030003' }];
-
-  const salesChannelOption =
-    [{ label: '선택없음', value: '' },
-    { label: '전화', value: '0040001' },
-    { label: '이메일', value: '0040002' },
-    { label: '대면', value: '0040003' },
-    { label: '행사참여', value: '0040004' },
-    { label: '온라인 리서치', value: '0040005' },
-    { label: '도서-전문정보', value: '0040006' },
-    { label: '소셜 커뮤니티', value: '0040007' },
-    { label: '기타', value: '0040008' }];
-
-  const leadActivityOption =
-    [{ label: '선택없음', value: '' },
-    { label: '발굴', value: '0020001' },
-    { label: '접촉', value: '0020002' },
-    { label: '제안', value: '0020003' },
-    { label: '검증', value: '0020004' }];
-
-  const NeedsOption =
-    [{ label: '선택없음', value: '' },
-    { label: '전략니즈', value: '전략' },
-    { label: '제품니즈', value: '제품' },
-    { label: '개인니즈', value: '개인' },
-    { label: '상품니즈', value: '상품' }];
-
 
   //부서별 사용자 조회
   const [data, setData] = useState({
@@ -159,9 +118,7 @@ const SalesLogFilterDash = (props) => {
       setSelectedOrganization(v);
       props.getorganizationusersDash({ dept_idx: 0, typ: 'tree' })
     }
-
   }
-
 
   function filterList(label) {
     let list = []
@@ -199,9 +156,6 @@ const SalesLogFilterDash = (props) => {
     return rtn;
   }
 
-
-  //const filteredlist = props.organizationuserDashRes && props.organizationuserDashRes.map(v => v.user_name);
-  //const filteredOptions = filteredlist && filteredlist.filter((v) => !selectedItems.includes(v)) 
   return (
     <>
       <Row gutter={6}>
@@ -225,7 +179,7 @@ const SalesLogFilterDash = (props) => {
             onChange={onOrganizationUserSelectChange}
             value={selectedItems}
             id={props.id}
-            maxTagCount={3} >
+            maxTagCount={isMobile ? 2 : 3} >
             {filteredOptions && filteredOptions.map((item, index) => (
               <Select.Option key={index} id={props.id} value={item}>
                 {item}
@@ -235,37 +189,6 @@ const SalesLogFilterDash = (props) => {
         </Col>
       </Row>
       <Row className='mt-1'></Row>
-      {/* <Row gutter={6}>
-        <Col sm={6} xs={6} md={6} lg={6}>
-          <Select placeholder='단계'
-            style={selectStyle}
-            options={leadActivityOption}
-            value={leadActivityOption.value}
-            onChange={onLeadActivity} />
-        </Col>
-        <Col sm={6} xs={6} md={6} lg={6}>
-          <Select placeholder='활동'
-            onChange={onSalesActivity}
-            options={salesActivityOption}
-            value={salesActivityOption.value}
-            style={selectStyle} />
-        </Col>
-        <Col sm={6} xs={6} md={6} lg={6}>
-          <Select placeholder='채널'
-            options={salesChannelOption}
-            value={salesChannelOption.value}
-            onChange={onSalesChannel}
-            style={selectStyle} />
-        </Col>
-        <Col sm={6} xs={6} md={6} lg={6}>
-          <Select placeholder='니즈'
-            options={NeedsOption}
-            value={NeedsOption.value}
-            onChange={onNeeds}
-            style={selectStyle} />
-
-        </Col>
-      </Row> */}
     </>
   )
 
