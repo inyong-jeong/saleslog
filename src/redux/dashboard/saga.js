55a812bceb4c709fa83daab84a14b5d54eeb6463
yechubot <yechukim217@gmail.com>
2021-10-18 11:24:01 +0900

    add: loading and hide msg on dashboard

--- src/redux/dashboard/saga.js
+++ src/redux/dashboard/saga.js
@@ -1,35 +1,44 @@
 import { all, call, fork, put, takeEvery } from 'redux-saga/effects'
-import { get_fetch, post_fetch, post_fetch_files } from 'model/FetchManage'
-import { errorMessage } from "constants/commonFunc";
+import { post_fetch } from 'model/FetchManage'
+
 import {
   GET_SALES_STAT,
   GET_LEAD_STAT
 } from 'constants/actionTypes'
+
 import {
-  getsaleslogstat, getleadlogstat
+  getsaleslogstat,
+  getleadlogstat
 } from './actions'
 
+import {
+  successMessage,
+  loadingMessage,
+  hideMessage,
+  errorMessage
+} from '../../constants/commonFunc'
+
 const cmm = require('constants/common');
-
-
-//customer api 
-const SALESLOG_STAT = '/saleslog/summary_saleslog'                    //saleslog
-const LEADLOG_STAT = '/saleslog/summary_saleslog_lead'                //leadlog
+const SALESLOG_STAT = '/saleslog/summary_saleslog'
+const LEADLOG_STAT = '/saleslog/summary_saleslog_lead'
 
 function* _getSalesLogStat({ payload: { body } }) {
   try {
-    //yield errorMessage('test');    
+    yield loadingMessage()
     const response = yield call(post_fetch, cmm.SERVER_API_URL + SALESLOG_STAT, body)
+    yield hideMessage()
     yield put(getsaleslogstat.success(response))
-    
-  } catch (error) {    
+
+  } catch (error) {
     yield put(getsaleslogstat.error(error))
   }
 }
 
 function* _getLeadLogStat({ payload: { body } }) {
   try {
+    yield loadingMessage()
     const response = yield call(post_fetch, cmm.SERVER_API_URL + LEADLOG_STAT, body)
+    yield hideMessage()
     yield put(getleadlogstat.success(response))
   } catch (error) {
     yield put(getleadlogstat.error(error))
