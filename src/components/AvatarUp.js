f106b8636860f33ff7239349da96d43488c037f4
yechubot <yechukim217@gmail.com>
2021-10-20 14:20:00 +0900

    style: fix wrong position and refactor

--- src/components/AvatarUp.js
+++ src/components/AvatarUp.js
@@ -2,12 +2,9 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Avatar } from 'antd';
 import { CameraOutlined, PlusOutlined } from '@ant-design/icons';
 
-// IconLabel
-// 아이콘 형태 - iconShape ('':원모양 / 'square':사각모양)
-// 아이콘 크기 - iconSize
 const AvatarUp = (props) => {
   const hiddenFileInput = useRef(null);
-  const [imgsrc, setImgsrc] = useState('');   // 이미지 경로  
+  const [imgsrc, setImgsrc] = useState('');
 
   useEffect(() => {
     setImgsrc(props.imgsrc);
@@ -18,54 +15,43 @@ const AvatarUp = (props) => {
   };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        verticalAlign: 'middle',
-        alignItems: 'center',
-      }}
-    >
-      {((imgsrc === undefined || imgsrc === '' || imgsrc === null) ? true : false) &&
-        <div style={{ justifyContent: 'center', alignItems: 'center' }}>
+    <div>
+      {
+        (imgsrc === undefined || imgsrc === '' || imgsrc === null) ?
           <Avatar
             shape={props.iconShape}
             size={(props.iconSize === undefined) ? 20 : props.iconSize}
             style={props.style}
-            onClick={handleClick}
-          >
-            <PlusOutlined
-              style={{
-                position: 'relative',
-                marginTop: 5,
-                top: (props.iconSize === undefined) ? props.height / 2 - 30 : 0,
-                fontSize: '50px',
-              }} />
+            onClick={handleClick} >
+            <div style={{ ...props.style, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <PlusOutlined style={{ fontSize: 25 }} />
+            </div>
           </Avatar>
-        </div>
+          :
+          <Avatar
+            shape={props.iconShape}
+            size={(props.iconSize === undefined) ? 20 : props.iconSize}
+            style={props.style}
+            src={props.imgsrc}
+          />
       }
-      {((imgsrc === undefined || imgsrc === '' || imgsrc === null) ? false : true) &&
-        <Avatar
-          shape={props.iconShape}
-          size={(props.iconSize === undefined) ? 20 : props.iconSize}
-          style={props.style}
-          src={props.imgsrc}
-        />
-      }
-      {/* 카메라 
-      height 를 prop으로 넘겨주세요!!
-      */}
-      {props.hideIcon ? '' : <Avatar size={26} style={{
-        position: 'relative',
-        left: -15,
-        top: props.height / 2,
-        backgroundColor: 'rgba(52, 52, 52, 0.5)',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-        onClick={handleClick}>
-        <CameraOutlined style={{ position: 'relative', left: 0, top: -5, fontSize: '13px' }} />
-      </Avatar>}
-      <div style={{ padding: 5, marginTop: 2 }}>{props.title}</div>
+
+
+      {// height prop으로 넘겨줄 것 
+        props.hideIcon ? null :
+          <Avatar
+            size={26}
+            style={{
+              position: 'relative',
+              left: -15,
+              top: props.height / 2,
+              backgroundColor: 'rgba(52, 52, 52, 0.5)',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+            onClick={handleClick}>
+            <CameraOutlined style={{ position: 'relative', left: 0, top: -5, fontSize: '13px' }} />
+          </Avatar>}
       <input
         type="file"
         ref={hiddenFileInput}
