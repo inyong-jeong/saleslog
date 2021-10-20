f95d9ffad6ddfb43009f4e2e7f32d8726628f7d4
yechubot <yechukim217@gmail.com>
2021-10-20 12:24:54 +0900

    add: privacy policy

--- src/pages/appInfo/index.js
+++ src/pages/appInfo/index.js
@@ -3,6 +3,7 @@ import MyAppBar from '../../components/styledcomponent/MyAppBar';
 import { useHistory } from 'react-router';
 import FullTabs from '../../components/styledcomponent/FullTabs';
 import Serviceinfo from './info';
+import PrivacyPolicy from './privacy';
 
 const { TabPane } = FullTabs;
 const AppInfo = () => {
@@ -10,15 +11,13 @@ const AppInfo = () => {
   const navigateTo = () => {
     history.goBack()
   }
-
   const onTabChange = () => {
-
   }
+
   return (
     <>
       <MyAppBar
         barTitle='앱 정보'
-        //showBackButton
         navigateTo={navigateTo}
       />
       <div className='content_body'>
@@ -26,8 +25,10 @@ const AppInfo = () => {
           <TabPane tab='서비스 이용약관' key='1'>
             <Serviceinfo />
           </TabPane>
-          {/* <TabPane tab='개인정보 처리방침' key='2'></TabPane>
-        <TabPane tab='위치서비스 약관' key='3'></TabPane> */}
+          <TabPane tab='개인정보처리방침' key='2'>
+            <PrivacyPolicy />
+          </TabPane>
+          {/* <TabPane tab='위치서비스 약관' key='3'></TabPane> */}
         </FullTabs>
       </div>
     </>
