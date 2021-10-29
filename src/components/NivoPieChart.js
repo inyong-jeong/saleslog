22890a8d33d07ca69b7291dc9a40f5717253a1fa
yechubot <yechukim217@gmail.com>
2021-10-29 10:44:17 +0900

    fix: chart length and hide legends

--- src/components/NivoPieChart.js
+++ src/components/NivoPieChart.js
@@ -24,6 +24,9 @@ const NivoPieChart = (props) => {
         // theme={theme}
         enableArcLabels={hasData}
         isInteractive={hasData}
+        arcLinkLabelsDiagonalLength={9}
+        arcLinkLabelsStraightLength={9}
+        arcLinkLabelsTextOffset={4}
         data={props.data}
         margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
         innerRadius={0.7}
@@ -39,32 +42,32 @@ const NivoPieChart = (props) => {
         arcLabelsSkipAngle={10}
         arcLabelsTextColor={{ from: 'color', modifiers: [['brighter', 2]] }}
         colors={colors(props.colors)}
-        legends={[
-          {
-            anchor: 'bottom',
-            direction: 'row',
-            justify: false,
-            translateX: 0,
-            translateY: 56,
-            itemsSpacing: 0,
-            itemWidth: 100,
-            itemHeight: 18,
-            itemTextColor: '#999999',
-            itemDirection: 'left-to-right',
-            itemOpacity: 1,
-            symbolSize: 18,
-            symbolShape: 'circle',
-            effects: [
-              {
-                on: 'hover',
-                style: {
-                  itemTextColor: '#000',
-                  cursor: 'pointer'
-                }
-              }
-            ]
-          }
-        ]}
+      // legends={[
+      //   {
+      //     anchor: 'bottom',
+      //     direction: 'row',
+      //     justify: false,
+      //     translateX: 0,
+      //     translateY: 56,
+      //     itemsSpacing: 0,
+      //     itemWidth: 100,
+      //     itemHeight: 18,
+      //     itemTextColor: '#999999',
+      //     itemDirection: 'left-to-right',
+      //     itemOpacity: 1,
+      //     symbolSize: 18,
+      //     symbolShape: 'circle',
+      //     effects: [
+      //       {
+      //         on: 'hover',
+      //         style: {
+      //           itemTextColor: '#000',
+      //           cursor: 'pointer'
+      //         }
+      //       }
+      //     ]
+      //   }
+      // ]}
       />
     </div>
   );
