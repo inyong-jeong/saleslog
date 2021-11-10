71f86d4dc1cc8c74e843a0b9961c703af03cda21
yechubot <yechukim217@gmail.com>
2021-11-10 13:22:55 +0900

    add: comment noti menu

--- src/components/styledcomponent/MyAppBar.js
+++ src/components/styledcomponent/MyAppBar.js
@@ -148,11 +148,11 @@ const MyAppBar = ({
         </Link>
       </Menu.Item>
       <Menu.Divider />
-      <Menu.Item key="4">
+      {/* <Menu.Item key="4">
         <Link to={'/main/notification'}>
           <Setting /> 알림 설정
         </Link>
-      </Menu.Item>
+      </Menu.Item> */}
       <Menu.Item key="5">
         <Link to={'/main/support'}>
           <Support /> 지원센터
